6f8287dc6a48189f760c7e80cb6f96bd79f7dd43
sulozx <sulozx@users.noreply.github.com>
2016-11-23 09:18:54 +0100

    Urls update for django 1.10

--- dajaxice/urls.py
+++ dajaxice/urls.py
@@ -1,11 +1,26 @@
+old_style_url = False
 try:
     from django.conf.urls import patterns, url
+    old_style_url = True
 except ImportError:
-    from django.conf.urls.defaults import patterns, url
+    try:
+        from django.conf.urls.defaults import patterns, url
+        old_style_url = True
+    except ImportError:
+        from django.conf.urls import url
 
 from .views import DajaxiceRequest
 
-urlpatterns = patterns('dajaxice.views',
-    url(r'^(.+)/$', DajaxiceRequest.as_view(), name='dajaxice-call-endpoint'),
-    url(r'', DajaxiceRequest.as_view(), name='dajaxice-endpoint'),
-)
+if old_style_url:
+    urlpatterns = patterns(
+        'dajaxice.views',
+        url(r'^(.+)/$', DajaxiceRequest.as_view(), name='dajaxice-call-endpoint'),
+        url(r'', DajaxiceRequest.as_view(), name='dajaxice-endpoint'),
+    )
+else:
+    urlpatterns = [
+        # 'dajaxice.views',
+        url(r'^(.+)/$', DajaxiceRequest.as_view(), name='dajaxice-call-endpoint'),
+        url(r'', DajaxiceRequest.as_view(), name='dajaxice-endpoint'),
+    ]
+
